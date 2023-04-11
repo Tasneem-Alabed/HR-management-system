ff1c09f7f9d761c4b06531e9f74ad27eb64347b6
Tasneem-Alabed <tsneemabedd@gmail.com>
2023-04-11 14:41:55 +0000

    complet solve the lab

--- JS/app.js
+++ JS/app.js
@@ -1,12 +1,17 @@
-'use strict'
-function Employee (employeeId , fullName , department , level , image , salary){
+'use strict';
+
+let employee = [];
+function Employee(employeeId , fullName , department , level , image , salary){
 this.Employee=employeeId;
 this.fullName=fullName;
 this.department=department;
 this.level=level;
+this.salary= 0;
+this.image=image;
+employee.push(this);
 }
 
-let ghazi =new Employee(1000 , "Ghazi Samer" , "Administration" , "Senior");
+let ghazi =new Employee(1000 , "Ghazi Samer" , "Administration" , "Senior"  );
  
 let lanl =new Employee(1001 , "Lana Ali","Finance","Senior");
 
@@ -19,3 +24,42 @@ let omar = new Employee(1004 , "Omar Zaid","Development","Senior");
 let rana = new Employee(1005 ,"Rana Saleh" ,"Development","Junior");
 
 let hadi= new Employee(1006 , "Hadi Ahmad","Finance","Mid-Senior");
+
+
+Employee.prototype.calculate = function() {
+ if(this.level == "Senior"){
+ this.salary = randomNamber(1500,2000) - (randomNamber(1500,2000) *0.075);
+ }else if(this.level=="Mid-Senior"){
+     this.salary=  randomNamber(1000,1500) -( randomNamber(1000,1500)* 0.075) ;
+ }else if(this.level=="Junior"){
+     this.salary=  randomNamber(500,1000) -(randomNamber(500,1000) * 0.075) ;
+ }
+}
+
+function randomNamber( min , max){
+return Math.floor(Math.random()) * (max-min)+min;
+
+}
+ghazi.calculate();
+tamara.calculate();
+lanl.calculate();
+safi.calculate();
+omar.calculate();
+rana.calculate();
+hadi.calculate(); 
+
+Employee.prototype.render =function(){
+   
+        document.write(`The name is : ${this.fullName}   the Salary    = ${this.salary}`);
+        document.write("<br>")
+        document.write("<hr>")
+
+    }
+ 
+ function renderEmployee(){
+    for(let i = 0 ; i<employee.length ; i++){
+        employee[i].render();
+
+    }
+ }
+ renderEmployee();
