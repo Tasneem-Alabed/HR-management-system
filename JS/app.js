4831c7191d76fbba1275c2125e3ddf191f8ae325
Tasneem-Alabed <tsneemabedd@gmail.com>
2023-04-13 15:17:38 +0000

    solve lab 8

--- JS/app.js
+++ JS/app.js
@@ -13,19 +13,19 @@ function Employee(employeeId, fullName, department, level, image, salary) {
     employee.push(this);
 }
 
-let ghazi = new Employee(1000, "Ghazi Samer", "Administration", "Senior");
+// let ghazi = new Employee(1000, "Ghazi Samer", "Administration", "Senior");
 
-let lanl = new Employee(1001, "Lana Ali", "Finance", "Senior");
+// let lanl = new Employee(1001, "Lana Ali", "Finance", "Senior");
 
-let tamara = new Employee(1002, "Tamara Ayoub", "Marketing", "Senior");
+// let tamara = new Employee(1002, "Tamara Ayoub", "Marketing", "Senior");
 
-let safi = new Employee(1003, "Safi Walid", "Administration", "Mid-Senior");
+// let safi = new Employee(1003, "Safi Walid", "Administration", "Mid-Senior");
 
-let omar = new Employee(1004, "Omar Zaid", "Development", "Senior");
+// let omar = new Employee(1004, "Omar Zaid", "Development", "Senior");
 
-let rana = new Employee(1005, "Rana Saleh", "Development", "Junior");
+// let rana = new Employee(1005, "Rana Saleh", "Development", "Junior");
 
-let hadi = new Employee(1006, "Hadi Ahmad", "Finance", "Mid-Senior");
+// let hadi = new Employee(1006, "Hadi Ahmad", "Finance", "Mid-Senior");
 
 
 Employee.prototype.calculate = function () {
@@ -55,100 +55,104 @@ function randomNamber(min, max) {
 
     return Math.floor(Math.random() * (max - min)) + min;
 
-}
-ghazi.calculate();
-tamara.calculate();
-lanl.calculate();
-safi.calculate();
-omar.calculate();
-rana.calculate();
-hadi.calculate();
+ }
+// ghazi.calculate();
+// tamara.calculate();
+// lanl.calculate();
+// safi.calculate();
+// omar.calculate();
+// rana.calculate();
+// hadi.calculate();
 
 
 
-Employee.prototype.render = function () {
+// Employee.prototype.render = function () {
 
-    document.write(`The name is : ${this.fullName}   the Salary = ${this.salary}  `);
-    const imgEl = document.createElement('img');
-    let secEL = document.getElementById("render");
-    secEL.appendChild(imgEl);
-    imgEl.src = this.image;
-    document.write("<br>")
-    document.write("<hr>")
+//     document.write(`The name is : ${this.fullName}   the Salary = ${this.salary}  `);
+//     const imgEl = document.createElement('img');
+//     let secEL = document.getElementById("render");
+//     secEL.appendChild(imgEl);
+//     imgEl.src = this.image;
+//     document.write("<br>")
+//     document.write("<hr>")
 
 
-}
-
-function renderEmployee() {
-    for (let i = 0; i < employee.length; i++) {
-        employee[i].render();
-
-    }
-}
-renderEmployee();
-
-// function generateNumber() {
-//     return Math.floor(Math.random() * (9000 + 1000) + 1000);
 // }
-// function calcoSalary(level) {
-//     if (level == "Senior") {
-//         return randomNamber(1500, 2000) - (randomNamber(1500, 2000) * 0.075);
-//     } else if (level == "Mid-Senior") {
-//         return randomNamber(1000, 1500) - (randomNamber(1000, 1500) * 0.075);
-//     } else if (level == "Junior") {
-//         return randomNamber(500, 1000) - (randomNamber(500, 1000) * 0.075);
+
+// function renderEmployee() {
+//     for (let i = 0; i < employee.length; i++) {
+//         employee[i].render();
+
 //     }
 // }
+// renderEmployee();
 
-// form.addEventListener('submit', gitData);
+function generateNumber() {
+    return Math.floor(Math.random() * (9000 + 1000) + 1000);
+}
+function calcoSalary(level) {
+    if (level == "Senior") {
+        return randomNamber(1500, 2000) - (randomNamber(1500, 2000) * 0.075);
+    } else if (level == "Mid-Senior") {
+        return randomNamber(1000, 1500) - (randomNamber(1000, 1500) * 0.075);
+    } else if (level == "Junior") {
+        return randomNamber(500, 1000) - (randomNamber(500, 1000) * 0.075);
+    }
+}
 
-// function gitData(event) {
-//     event.preventDefault();
-//     let name = (event.target.name.value);
-//     let id = generateNumber();
-//     let department = (event.target.dep.value);
-//     let level = (event.target.level.value);
-//     let salary = calcoSalary(level);
-//     let img = (event.target.img.value);
+form.addEventListener('submit', gitData);
 
-//     rendEmployee(name, id, department, level, salary, img);
-// }
-// function rendEmployee(name, id, department, level, salary, img) {
+function gitData(event) {
+    event.preventDefault();
+    let name = (event.target.name.value);
+    let id = generateNumber();
+    let department = (event.target.dep.value);
+    let level = (event.target.level.value);
+    let salary = calcoSalary(level);
+    let img = (event.target.img.value);
 
-
-//     const seEl = document.getElementById("rendEmployee");
-
-//     const divEl = document.createElement("div");
-
-//     seEl.appendChild(divEl);
-//     const imgEl = document.createElement("img");
-//     divEl.appendChild(imgEl);
-//     imgEl.src =`./img/${name}.jpg`;
-
-//     const pEl = document.createElement("p");
-//     divEl.appendChild(pEl);
-//     pEl.textContent=name;
-
-//     const p2El =document.createElement("p");
-//     divEl.appendChild(p2El);
-//     p2El.textContent=id;
-
-//     const p3El = document.createElement("p");
-//     divEl.appendChild(p3El);
-//      p3El.textContent=department;
-
-//     const p4El=document.createElement("p");
-//     divEl.appendChild(p4El);
-//     p4El.textContent= level;
-
-//     const p5El=document.createElement("p");
-//     divEl.appendChild(p5El);
-//     p5El.textContent=salary;
+    rendEmployee(name, id, department, level, salary, img);
+}
+function rendEmployee(name, id, department, level, salary, img) {
 
 
+    const seEl = document.getElementById("rendEmployee");
+
+    const divEl = document.createElement("div");
+
+    seEl.appendChild(divEl);
+    const imgEl = document.createElement("img");
+    divEl.appendChild(imgEl);
+    imgEl.src =`./img/${name}.jpg`;
+
+    const div2El =document.createElement("divEl");
+    divEl.appendChild( div2El);
+
+    const pEl = document.createElement("p");
+    divEl.appendChild(pEl);
+    pEl.textContent="Name :"+name;
+
+    const p2El =document.createElement("p");
+    divEl.appendChild(p2El);
+    p2El.textContent="Id :"+id;
+
+  
+    const p3El = document.createElement("p");
+    divEl.appendChild(p3El);
+     p3El.textContent="Department : "+department;
+
+    const p4El=document.createElement("p");
+    divEl.appendChild(p4El);
+    p4El.textContent= "Level : "+level;
+
+    const p5El=document.createElement("p");
+    divEl.appendChild(p5El);
+    p5El.textContent="Salary : "+salary +"$";
 
 
-// }
 
-// generateNumber();
+
+}
+
+generateNumber();
 
